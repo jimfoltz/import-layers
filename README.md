a0fe581682c751e8a5e0d9a15f927c455f31c0e2
Jim Foltz <jim.foltz@gmail.com>
2017-02-11 10:25:32 -0500

    add ruby code

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # import-layers
 
 Demo extension for SketchUp to grab Layers froman external model. 
+
+This currently is set up *only* for SketchUp 2016 on Windows. 
+
+
---
+++ ruby_extension/jf_import_layers.rb
@@ -0,0 +1,28 @@
+
+if RUBY_PLATFORM[/mswin/]  && Sketchup.version.to_i >= 15 && Sketchup.is_64bit? && RUBY_VERSION == "2.0.0"
+
+   require_relative  "jf_import_layers/2.0.0/x64/JF_ImportLayers.so"
+
+   module JF
+      module ImportLayers
+
+         menu = defined?(JF.menu) ? JF.menu : UI.menu
+         menu.add_item("Import Layers") { main }
+
+         def self.main
+            file = UI.openpanel("Select SketchUp Model", nil, "*.skp")
+            if file && File.exists?(file)
+               info = get_layer_info(file)
+               Sketchup.active_model.start_operation("Import Layers", true)
+               info.each {|name, color|
+                  layer = Sketchup.active_model.layers.add(name)
+                  layer.color = color
+               }
+               Sketchup.active_model.commit_operation
+            end
+         end
+      end
+
+   end
+
+end
